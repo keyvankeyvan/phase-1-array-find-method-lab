683f3c668251d7465625623abbf18f551e8e2d30
Keyvan Alonso <keyvanalonso@gmail.com>
2022-03-28 01:39:37 -0400

    commit tree ff834b3f337cdf63cf11f9bd0c9dd21f60d5bc63

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+/* function superbowlWin(record) {
+    for (const results of record){
+        if (results.result ==='W'){
+            return results.year
+        }
+    }
+} */
+
+function finder(obj){
+    return obj.result === 'W'
+}
+
+function superbowlWin(record) {
+    const winner = record.find(finder)
+    if (winner != undefined) {
+        return winner.year
+    }
+}
